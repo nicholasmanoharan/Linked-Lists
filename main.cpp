4b47b91a55322d16f0a60c8bd4c374f0a1ea2f2e
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2023-12-05 20:44:26 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include "Node.hpp"
-#include "Student.hpp"
+#include "Student.hpp"  // Make sure the correct header file is included
 
 int main() {
     Student* student1 = new Student("Student 1", 1);
     Student* student2 = new Student("Student 2", 2);
     Node* node1 = new Node(student1);
-    Node* node2 = new Ndoe(student2);
-    
-    node1 -> setNext(node2);
-    
+    Node* node2 = new Node(student2);
+
+    node1->setNext(node2);
+
     Node* currentNode = node1;
-    while (curretNode != nullptr) {
-        Student* currentStudent = currentNode -> getStudent();
-        currentStudent -> displayInfo();
-        currentNode = currentNode -> getNext();
+    while (currentNode != nullptr) {
+        Student* currentStudent = currentNode->getStudent();
+        currentStudent->displayInfo();
+        currentNode = currentNode->getNext();
     }
-    
-    //cleaning up memory
+
+    // Cleaning up memory
     delete node1;
     delete node2;
     delete student1;
     delete student2;
-    
+
     return 0;
-    
 }
